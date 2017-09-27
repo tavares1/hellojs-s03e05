e4a19af209a7a2f4baf8c80581aa8731f73c2543
tavares <tavares@localhost.localdomain>
2017-09-26 23:55:14 -0300

    desenvolvendo butão de del

--- index.js
+++ index.js
@@ -9,12 +9,17 @@ app.use(express.static("public"))
 
 app.get("/list", (req, res) => 
   knex("contato").select().then(ret => res.send(ret)))
-app.post("/save", (req, res) => 
-  knex("contato").insert(req.body, "idcontato").then(ret => res.send("OK-" + ret)))
+app.post("/save", (req, res) => {
+  console.log(req.body)
+  knex("contato").insert(req.body, "idcontato").then(ret => res.send("OK-" + ret))})
 app.put("/save", (req, res) => 
   knex("contato").update(req.body)
   .where("idcontato", req.body.idcontato).then(ret => res.send("OK-" + ret)))
+app.delete("/delete",(req,res) => {
+  console.log(req.body)
+  knex("contato").where("idcontato",req.body.idcontato).del()
+})
 
-knex.migrate.latest().then(_ => app.listen(3000))
+knex.migrate.latest().then(_ => app.listen(4000))
 
 console.log("ctrl+c to quit")
